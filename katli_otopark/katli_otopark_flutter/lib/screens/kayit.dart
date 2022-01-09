cdf86eea674ce87825121243a329997f2c436a83
Selim Kosgen <selimkosgen1999@gmail.com>
2022-01-09 14:03:50 +0300

    cloudstore eklendi

--- katli_otopark/katli_otopark_flutter/lib/screens/kayit.dart
+++ katli_otopark/katli_otopark_flutter/lib/screens/kayit.dart
@@ -1,155 +1,151 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:get/get.dart';
-import 'package:katli_otopark_flutter/controllers/arabagirisi_controller.dart';
-import 'package:katli_otopark_flutter/models/arabagirisi.dart';
-import 'package:katli_otopark_flutter/widgets/button.dart';
-import 'package:katli_otopark_flutter/widgets/input_field.dart';
-import 'package:katli_otopark_flutter/screens/kayitlar.dart';
-
-
+import 'package:katliotopark/controllers/arabagirisi_controller.dart';
+import 'package:katliotopark/models/arabagirisi.dart';
+import 'package:katliotopark/widgets/button.dart';
+import 'package:katliotopark/widgets/input_field.dart';
+import 'package:katliotopark/screens/kayitlar.dart';
 
 class kayitPage extends StatefulWidget {
   @override
   _kayitPageState createState() => _kayitPageState();
 }
-class _kayitPageState extends State<kayitPage> {
 
+class _kayitPageState extends State<kayitPage> {
   final TaskController _taskController = Get.put(TaskController());
-  final TextEditingController _markaController =TextEditingController();
-  final TextEditingController _plakaController =TextEditingController();
+  final TextEditingController _markaController = TextEditingController();
+  final TextEditingController _plakaController = TextEditingController();
   String _endTime = "10:00 AM ";
-  String _startTime= DateFormat("hh:mm a").format(DateTime.now()).toString();
-  
+  String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
+
+  var taskCollections = FirebaseFirestore.instance.collection("parkLots");
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(backgroundColor: Color(0xFFD40700),),
-      body: Container(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            
-            children: [
-              Text("Araba Girişi",
-              style: TextStyle(fontWeight:
-               FontWeight.bold,fontSize: 25),
-               ),
-                MyInputField(title: "Plaka", hint: "Plakayı Girin",controller: _plakaController,),
-                MyInputField(title: "Marka", hint: "Markayı Girin",controller: _markaController,),
-                Row(
-                  children: [
-                    Expanded(
-                      child:
-                       MyInputField(
-                        title: "Giriş Saati", hint: _startTime,              
-                    ),
-                    ),
-                     IconButton(   
-                        onPressed: (){     
-                          _getTimeFromUser(isStartTime: true);
-                        },
-                      icon: Icon(
-                        Icons.access_time_rounded,
-                        color:Colors.grey[700],size: 20,
-                         ), 
-                      ),
-                      Expanded(
-                      child: MyInputField(
-                        title: "Çıkış Saati",
-                         hint: _endTime,
-                     
-                    ),
-                    
-                    ),
-                     IconButton(   
-                        onPressed: (){ 
-                          _getTimeFromUser(isStartTime: false);
-                        },
-                      icon: Icon(
-                        Icons.access_time_rounded,
-                        color:Colors.grey[700],size: 20,
-                         ), 
-                      ),
-                  ]
-                ), 
-                SizedBox(height: 20),
-                MyButton(label: "Kaydet", onTap: ()=>_validateDate())
-            ],
-            
-          ),
-        )
+      appBar: AppBar(
+        backgroundColor: Color(0xFFD40700),
       ),
+      body: Container(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Araba Girişi",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+                MyInputField(
+                  title: "Plaka",
+                  hint: "Plakayı Girin",
+                  controller: _plakaController,
+                ),
+                MyInputField(
+                  title: "Marka",
+                  hint: "Markayı Girin",
+                  controller: _markaController,
+                ),
+                Row(children: [
+                  Expanded(
+                    child: MyInputField(
+                      title: "Giriş Saati",
+                      hint: _startTime,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      _getTimeFromUser(isStartTime: true);
+                    },
+                    icon: Icon(
+                      Icons.access_time_rounded,
+                      color: Colors.grey[700],
+                      size: 20,
+                    ),
+                  ),
+                  Expanded(
+                    child: MyInputField(
+                      title: "Çıkış Saati",
+                      hint: _endTime,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      _getTimeFromUser(isStartTime: false);
+                    },
+                    icon: Icon(
+                      Icons.access_time_rounded,
+                      color: Colors.grey[700],
+                      size: 20,
+                    ),
+                  ),
+                ]),
+                SizedBox(height: 20),
+                MyButton(label: "Kaydet", onTap: () => _validateDate())
+              ],
+            ),
+          )),
     );
   }
- 
-    _validateDate(){
-    if(_plakaController.text.isNotEmpty&&_markaController.text.isNotEmpty){
+
+  _validateDate() {
+    if (_plakaController.text.isNotEmpty && _markaController.text.isNotEmpty) {
       _addTaskToDb();
       _taskController.getTasks();
-    Get.to(() => kayitlarPage());
+      taskCollections.doc(_plakaController.text.toString()).set({
+        "plaka": _plakaController.text,
+        "marka": _markaController.text,
+        "dateoftime": _startTime,
+      });
+      Get.to(() => kayitlarPage());
+    } else if (_plakaController.text.isEmpty || _markaController.text.isEmpty) {
+      Get.snackbar(
+        "Zorunlu",
+        "Bütün Alanların Doldurulması Zorunludur",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.white,
+        colorText: Colors.pink,
+        icon: Icon(Icons.warning_amber_rounded, color: Colors.red),
+      );
     }
-    else if (_plakaController.text.isEmpty ||_markaController.text.isEmpty){
-      Get.snackbar("Zorunlu", "Bütün Alanların Doldurulması Zorunludur",
-      snackPosition: SnackPosition.BOTTOM,
-      backgroundColor: Colors.white,
-      colorText: Colors.pink,
-      icon: Icon(Icons.warning_amber_rounded,
-      color:Colors.red),
-      
-       );
-
-    }
-
   }
-  _addTaskToDb()async{
-   int value = await _taskController.addTask(
-      task: Task(
+
+  _addTaskToDb() async {
+    int value = await _taskController.addTask(
+        task: Task(
       plaka: _plakaController.text,
       marka: _markaController.text,
       startTime: _startTime,
       endTime: _endTime,
-    
-    )
-   );
-   print ("My id is "+"$value");
-
-  }
-  
-  
-  _getTimeFromUser({required bool isStartTime}) async{
-   var pickedTime = await  _showTimePicker();
-   String _formatedTime = pickedTime.format(context);
-   if(pickedTime==null){
-     print("Saat iptal edildi");
-   }else if(isStartTime==true){
-     setState(() {
-       _startTime=_formatedTime;
-     });
-
-   }else if(isStartTime==false){
-     setState(() {
-       _endTime=_formatedTime;
-     });
-
-   }
+    ));
+    print("My id is " + "$value");
   }
 
-  _showTimePicker(){
-     return  showTimePicker(
-       initialEntryMode: TimePickerEntryMode.input,
-       context: context,
-       initialTime: TimeOfDay(
-        hour: int.parse(_startTime.split(":")[0]),
-        minute: int.parse(_startTime.split(":")[1].split(" ")[0]),
-        
-        
-        
-        )
-        );
+  _getTimeFromUser({required bool isStartTime}) async {
+    var pickedTime = await _showTimePicker();
+    String _formatedTime = pickedTime.format(context);
+    if (pickedTime == null) {
+      print("Saat iptal edildi");
+    } else if (isStartTime == true) {
+      setState(() {
+        _startTime = _formatedTime;
+      });
+    } else if (isStartTime == false) {
+      setState(() {
+        _endTime = _formatedTime;
+      });
+    }
   }
 
+  _showTimePicker() {
+    return showTimePicker(
+        initialEntryMode: TimePickerEntryMode.input,
+        context: context,
+        initialTime: TimeOfDay(
+          hour: int.parse(_startTime.split(":")[0]),
+          minute: int.parse(_startTime.split(":")[1].split(" ")[0]),
+        ));
   }
+}
--- katli_otopark/katli_otopark_flutter/lib/screens/kayitlar.dart
+++ katli_otopark/katli_otopark_flutter/lib/screens/kayitlar.dart
@@ -1,18 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
-import 'package:katli_otopark_flutter/controllers/arabagirisi_controller.dart';
-import 'package:katli_otopark_flutter/models/arabagirisi.dart';
-import 'package:katli_otopark_flutter/otopark.dart';
-import 'package:katli_otopark_flutter/screens/kayit.dart';
-import 'package:katli_otopark_flutter/ui/theme.dart';
-import 'package:katli_otopark_flutter/widgets/button.dart';
-import 'package:katli_otopark_flutter/widgets/task_tile.dart';
-
+import 'package:katliotopark/controllers/arabagirisi_controller.dart';
+import 'package:katliotopark/models/arabagirisi.dart';
+import 'package:katliotopark/otopark.dart';
+import 'package:katliotopark/screens/kayit.dart';
+import 'package:katliotopark/ui/theme.dart';
+import 'package:katliotopark/widgets/button.dart';
+import 'package:katliotopark/widgets/task_tile.dart';
 
 class kayitlarPage extends StatefulWidget {
-  const kayitlarPage({ Key? key }) : super(key: key);
+  const kayitlarPage({Key? key}) : super(key: key);
 
   @override
   _kayitlarPageState createState() => _kayitlarPageState();
@@ -20,196 +20,122 @@ class kayitlarPage extends StatefulWidget {
 
 class _kayitlarPageState extends State<kayitlarPage> {
   final _taskController = Get.put(TaskController());
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Color(0xFFD40700),
-      title: Text("Kayıtlı Araçlar"),),
-      body: Column(
-        children: [
-          _addKayitBar(),
-          SizedBox(height: 20,),
-           Container(
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            height: 0.6,
-            width: 400,
-            color: Colors.black87,
-          ),
-          SizedBox(height: 10),
-          _showTasks()
-         
-        
-        ],
-      ),
-      
-    );
-  }
-_showTasks(){
-  return Expanded(
-    child:Obx((){
-      return ListView.builder(
-        itemCount: _taskController.taskList.length,
-
-        itemBuilder: (_, index){
-        print(_taskController.taskList.length);
-          
-            
-            return AnimationConfiguration.staggeredList(
-              position: index,
-               child: SlideAnimation(
-                 child: FadeInAnimation(
-                   child: Row(
-                     children: [
-                       GestureDetector(
-                         onTap:(){
-                           _showBottomSheet(context, _taskController.taskList[index]);
-                         },
-                         child:TaskTile(_taskController.taskList[index]),
-                       )
-                     ],
-                   ),
-                 ),
-
-               )
-               );
-          
-
-  }
-  );
-
-     }),
-     );
-}
-
-    _showBottomSheet(BuildContext context, Task task){
-      Get.bottomSheet(
-        Container(
-          padding: const EdgeInsets.only(top: 4),
-          height: task.isComplated==1?
-          MediaQuery.of(context).size.height*0.24:
-          MediaQuery.of(context).size.height*0.32,
-          color: Colors.white,
-          child: Column(
-            children: [
-              Container(
-               height:6,
-               width: 200,
-               decoration:BoxDecoration(
-                borderRadius:BorderRadius.circular(10),
-                color: Colors.grey[600]
-
-              )
-              ),
-              Spacer(),
-              task.isComplated==1
-              ?Container()
-                 : _bottomSheetButton(
-                   label: "Ücret Ödendi",
-                   onTap: (){
-                     Get.back();
-                     },
-                   clr: blueishClr,
-                   
-                   context:context
-                   ),
-                    _bottomSheetButton(
-                   label: "Kaydı Sil",
-                   onTap: (){
-                     _taskController.delete(task);
-                     _taskController.getTasks();
-                     Get.back();
-                     },
-                   clr: Colors.red[300]!,
-                   
-                   context:context
-                   ),
-                   SizedBox(height: 10,),
-                   _bottomSheetButton(
-                   label: "Geri",
-                   onTap: (){
-                     Get.back();
-                     },
-                   clr: Colors.red[300]!,
-                   isClose: true,
-                   
-                   context:context
-                   ),
-                   SizedBox(height: 1,)
-              ],
-
-
-          ),
-
-        )
-      );
-      
-    }
-    _bottomSheetButton({
-
-      required String label,
-      required Function()? onTap,
-      required Color clr,
-      bool isClose=false,
-      required BuildContext context,
-    }){
-        return GestureDetector(
-          onTap: onTap,
-          child: Container(
-            margin: const EdgeInsets.symmetric(vertical: 4),
-            height: 55,
-            width: MediaQuery.of(context).size.width*0.9,
-            
-            decoration: BoxDecoration(
-              border:Border.all(
-                width:2,
-                color: isClose==true?Colors.red:clr
-              ),
-              borderRadius: BorderRadius.circular(20),
-              color:isClose==true?Colors.transparent:clr ,
-            ),
-
-            child: Center(
-              child: Text(
-                label,
-                style:isClose?titleStyle:titleStyle.copyWith(color: Colors.white )
-              ),
-            ),
-
-          ),
-
-        );
-    }
-
-  _addKayitBar(){  
-    return Container(
-            margin: const EdgeInsets.only(right: 10,top: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                child:
-                 Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(DateFormat.yMMMd().format(DateTime.now()),
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),),
-                    Text("Bugün",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22),)
-                    
-                  ],
-                ),
-              ),
-              
-              
-              ],
-              
-            ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("parkLots").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return new Text('Loading...');
+          return new ListView(
+            children: snapshot.data!.docs.map((document) {
+              return Container(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Card(
+                        color: Colors.grey,
+                        child: Card(
+                            color: Colors.grey,
+                            child: Stack(
+                              children: [
+                                Row(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: GestureDetector(
+                                        onTap: () {},
+                                        child: Container(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.25,
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.3,
+                                        ),
+                                      ),
+                                    ),
+                                    Column(
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.all(10.0),
+                                          child: Container(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.5,
+                                            child: Align(
+                                              alignment: Alignment.topLeft,
+                                              child: Text(
+                                                document["plaka"],
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 16,
+                                                    fontFamily: "NotoSerif"),
+                                                textAlign: TextAlign.justify,
+                                                maxLines: 2,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.5,
+                                          child: Align(
+                                            alignment: Alignment.topLeft,
+                                            child: Text(
+                                              "araç: " + document["marka"],
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 12,
+                                                  fontFamily: "NotoSerif"),
+                                              textAlign: TextAlign.justify,
+                                              maxLines: 1,
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(height: 10),
+                                        Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.5,
+                                          child: Align(
+                                            alignment: Alignment.topLeft,
+                                            child: Row(
+                                              children: [
+                                                Text(
+                                                  "Çıkış Yapacak: " +
+                                                      document["dateoftime"]
+                                                          .toString(),
+                                                  style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 14,
+                                                      fontFamily: "NotoSerif"),
+                                                  textAlign: TextAlign.justify,
+                                                  maxLines: 1,
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            )),
+                      )));
+            }).toList(),
           );
-           
-    
+        },
+      ),
+    );
   }
 }
